98f4b522d01afebc57d425d238adf440c115d277
Anish Thiriveedhi <thiri.anish@gmail.com>
2023-04-17 20:23:07 -0500

    Update code

--- server/app.py
+++ server/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+import csv
+
 
 app = Flask(__name__)
 
@@ -13,6 +15,16 @@ def post_number():
     dir = str(data['direction'])
     if not isinstance(dir, str):
         return jsonify({"error": "Invalid 'direction' value"}), 400
+    
+    # Append the received data to a CSV file
+    with open('dirs.csv', 'a', newline='') as csvfile:
+        fieldnames = ['direction']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+        # Uncomment the following line if you want to write the header for the first time
+        # writer.writeheader()
+        
+        writer.writerow({'direction': dir})
 
     return jsonify({"result": dir})
 
